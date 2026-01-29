204f075f8c2a616a1a62736c208f3dd2433d957c
Joshua Mandelson <joshmandelson5@gmail.com>
2026-01-29 17:28:59 -0600

    add launch screen

---
+++ iOS_Pokedex/View/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  launchscreen.swift
+//  iOS_Pokedex
+//
+//  Created by Joshua Mandelson on 1/29/26.
+//
+
+import SwiftUI
+
+struct launchscreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    launchscreen()
+}
